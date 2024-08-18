e43255503ad51d8ff5a71b5a925852fd2ec397ea
vinod4232 <ydv911634@gmail.com>
2024-08-18 20:06:08 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "hyperchain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "HYPERSDK"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "HY"
 )
 
 var ID ids.ID
